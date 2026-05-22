9c5309adb1c43ad4934f282a75100adc84502e0b
Samuel Ernesto Mercado Mercado <158174038+Samu2404@users.noreply.github.com>
2026-05-21 22:12:25 -0500

    Refactorizacion del codigo
    Se añadieron nuevas funciones a la libreria y se organizo su codigo para mejorar legibilidad. 
    
    La funcion agregada permite el envio de mensajes unicamente al momento en que se recibe TX=ON

--- Librerias/master_cel_coms/bluetooth_spp.h
+++ Librerias/master_cel_coms/bluetooth_spp.h
@@ -2,11 +2,9 @@
 #define BLUETOOTH_SPP_H
 #include <stdbool.h>
 #include <stdint.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 typedef struct {
     bool status_wifi_init;
     bool status_espnow_init;
@@ -21,39 +19,16 @@ typedef struct {
     bool status_lectura;
 } logs_status_t;
 
-/* --- Estructuras de Datos de Logs --- */
-
-
-/* --- API de Comunicación --- */
-
-/**
- * @brief Inicializa el stack BLE, servicios GATT (NUS) y comienza el advertising.
- * @note Requiere que nvs_flash_init() haya sido llamado previamente.
- */
+typedef void (*ble_tx_ready_callback_t)(bool tx_ready, void *ctx);
 void bluetooth_init(void);
-
-/**
- * @brief Formatea y envía un reporte de inicialización a la tablet.
- * @param data Puntero a la estructura de inicialización.
- */
 void ble_send_init_report(const logs_inits_t *data);
-
-/**
- * @brief Formatea y envía un reporte de estado en tiempo real.
- * @param data Puntero a la estructura de estado operativo.
- */
 void ble_send_runtime_report(const logs_status_t *data);
-
-/**
- * @brief Envía un mensaje de texto plano genérico vía BLE.
- * @param msg Cadena de caracteres terminada en nulo.
- */
 void nus_send_response(const char *msg);
-
+bool ble_is_tx_ready(void);
+void ble_register_tx_ready_callback(ble_tx_ready_callback_t callback, void *ctx);
 void ble_send_log_inits(const logs_inits_t *inits);
 void ble_send_log_status(const logs_status_t *status);
 #ifdef __cplusplus
 }
 #endif
-
 #endif // BLE_LOG_LIB_H
