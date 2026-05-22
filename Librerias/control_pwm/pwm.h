8d5b9bf8f5897a95d95c37c070f0408642cd22fc
Samuel Ernesto Mercado Mercado <158174038+Samu2404@users.noreply.github.com>
2026-05-21 22:08:14 -0500

    Refactorizacion del codigo
    Se refactorizo el código para mejorar la organización y optimizar el uso de variables y bibliotecas. Asimismo  ,se agregaron etiquetas a las funciones.

--- Librerias/control_pwm/pwm.h
+++ Librerias/control_pwm/pwm.h
@@ -2,14 +2,12 @@
 #define PWM_H
 
 #include <stdbool.h>
-#include <stdbool.h>
-
-
+#include <stdint.h>
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 void configuracion_pwm(gpio_num_t pin, ledc_channel_t canal, ledc_timer_t timer, int frecuencia, ledc_timer_bit_t resolucion_bits, bool *status_pwm_init);
-int clamp_int(int *valor, int min, int max);
-void setear_duty_cycle(int duty_cycle, bool *status);
+void setear_duty_cycle(uint32_t duty, bool *status);
 void encender_pwm_suave(uint32_t tiempo_ms, uint32_t pasos, bool *status_pwm);
 void apagar_pwm_suave(uint32_t tiempo_ms, uint32_t pasos, bool *status_pwm);
 
