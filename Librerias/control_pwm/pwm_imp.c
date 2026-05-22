722828b69eef965fe3e5e761eddf31d76dd5ddce
Samuel Ernesto Mercado Mercado <158174038+Samu2404@users.noreply.github.com>
2026-05-21 22:06:34 -0500

    Refactorizacion del codigo
    Se refactorizo el código para mejorar la organización y optimizar el uso de variables y bibliotecas. Asimismo  ,se agregaron etiquetas a las funciones.

--- Librerias/control_pwm/pwm_imp.c
+++ Librerias/control_pwm/pwm_imp.c
@@ -1,28 +1,21 @@
 #include "pwm.h"
-
-#include <stdbool.h>
-
-#include "driver/ledc.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define PWM_SPEED_MODE LEDC_LOW_SPEED_MODE
 
-static const char *TAG = "PWM";
+// Variable de configuracion del PWM
+#define PWM_SPEED_MODE LEDC_LOW_SPEED_MODE
 
 static ledc_channel_t canal_pwm;
 static ledc_timer_t timer_pwm;
 static gpio_num_t pin_pwm = GPIO_NUM_NC;
-
 static bool pwm_configurado = false;
 static uint32_t duty_maximo = 0;
 
-/* =========================================================
-   FUNCION AUXILIAR
-========================================================= */
 
+// Funcion para limitar un valor entero dentro de un rango
 static int clamp_int(int valor, int min, int max)
 {
     if (valor < min) return min;
@@ -31,10 +24,8 @@ static int clamp_int(int valor, int min, int max)
     return valor;
 }
 
-/* =========================================================
-   CONFIGURACION PWM
-========================================================= */
 
+// Configuracion pwm
 void configuracion_pwm(    gpio_num_t pin,    ledc_channel_t canal,    ledc_timer_t timer,    int frecuencia,    ledc_timer_bit_t resolucion_bits,    bool *status_pwm_init)
 {
     if (status_pwm_init != NULL) {
@@ -55,7 +46,6 @@ void configuracion_pwm(    gpio_num_t pin,    ledc_channel_t canal,    ledc_time
 
     esp_err_t rc = ledc_timer_config(&timer_conf);
 
-
     /* Configuracion canal */
     ledc_channel_config_t canal_conf = {
         .gpio_num = pin_pwm,
@@ -66,10 +56,7 @@ void configuracion_pwm(    gpio_num_t pin,    ledc_channel_t canal,    ledc_time
         .duty = 0,
         .hpoint = 0
     };
-
     rc = ledc_channel_config(&canal_conf);
-
-
 
     duty_maximo = (1 << resolucion_bits) - 1;
     pwm_configurado = true;
@@ -78,78 +65,56 @@ void configuracion_pwm(    gpio_num_t pin,    ledc_channel_t canal,    ledc_time
 }
 }
 
+
+// Funcion para establecer el duty cycle del PWM
 void setear_duty_cycle(uint32_t duty, bool *status)
 {
     if (status != NULL) {
         *status = false;
     }
-
     duty = clamp_int(duty, 0, duty_maximo);
-
     esp_err_t rc = ledc_set_duty(
         PWM_SPEED_MODE,
         canal_pwm,
         duty);
-
-
     rc = ledc_update_duty( PWM_SPEED_MODE, canal_pwm);
-
     if (status != NULL) {
         *status = true;
     }
 }
 
 
+// Encender el PWM de forma suave
 void encender_pwm_suave(    uint32_t tiempo_ms,    uint32_t pasos,    bool *status_pwm){
     if (status_pwm != NULL) {
         *status_pwm = false;
     }
-
     uint32_t delay_ms = tiempo_ms / pasos;
-
     for (uint32_t i = 0; i <= pasos; i++) {
-
         uint32_t duty = (i * duty_maximo) / pasos;
-
         bool status = false;
-
         setear_duty_cycle(duty, &status);
-
         vTaskDelay(pdMS_TO_TICKS(delay_ms));
     }
-
     if (status_pwm != NULL) {
         *status_pwm = true;
     }
 }
 
 
-
+// Funcion para apagar el PWM de forma suave
 void apagar_pwm_suave(    uint32_t tiempo_ms,    uint32_t pasos,    bool *status_pwm)
 {
     if (status_pwm != NULL) {
         *status_pwm = false;
     }
-
     uint32_t delay_ms = tiempo_ms / pasos;
-
     for (int i = pasos; i >= 0; i--) {
-
         uint32_t duty = (i * duty_maximo) / pasos;
-
         bool status = false;
-
         setear_duty_cycle(duty, &status);
-
-        if (!status) {
-            ESP_LOGE(TAG, "Fallo durante apagado suave");
-            return;
-        }
-
         vTaskDelay(pdMS_TO_TICKS(delay_ms));
     }
-
-
     if (status_pwm != NULL) {
         *status_pwm = true;
     }
