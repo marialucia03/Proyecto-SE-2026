0065a29b7daf8a1b9ede82a4332c0680ad6e2302
Samuel Ernesto Mercado Mercado <158174038+Samu2404@users.noreply.github.com>
2026-05-21 22:16:23 -0500

    Refactor de datos_t  y limpieza del header

--- Librerias/master_slave_coms/comunicaciones.h
+++ Librerias/master_slave_coms/comunicaciones.h
@@ -7,13 +7,15 @@
 #include "esp_log.h"
 #include "esp_now.h"
 
+
+// Estructura para los datos que se enviarán entre el maestro y el esclavo
 typedef struct {
-    int voltaje_potenciometro;
     int temperatura;
     int bpm;
 } datos_t;
 
 
+// Funciones para la configuración y manejo de comunicaciones WiFi y ESPNOW
 void configuracion_wifi(int canal,bool *status_wifi_init);
 void configuracion_espnow(int canal, const uint8_t *peer_mac, bool *status_espnow_init);
 void enviar_datos(const datos_t *datos, const uint8_t *peer_mac);
@@ -21,8 +23,9 @@ void recibir_datos(const esp_now_recv_info_t *recv_info, const uint8_t *data, in
 void estado_conexion_ping(const uint8_t *peer_mac);
 void estado_conexion_pong (const uint8_t *peer_mac);
 void ping_pong(const uint8_t *peer_mac, bool *status_conexion, int gpio_led_pin, bool MS);
-// Devuelve una copia del último paquete `datos_t` recibido vía ESP-NOW
 void obtener_ultimo_datos(datos_t *out);
 
 #endif
+
+
 
